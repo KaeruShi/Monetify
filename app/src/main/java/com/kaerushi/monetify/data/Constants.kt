e36631063c9ae0741f42353681c11d7b58606a89
KaeruShi <farel.ardiansyah404@gmail.com>
2026-02-09 13:40:25 +0700

    Fix package name const

--- app/src/main/java/com/kaerushi/monetify/data/Constants.kt
+++ app/src/main/java/com/kaerushi/monetify/data/Constants.kt
@@ -1,7 +1,9 @@
 package com.kaerushi.monetify.data
 
+import com.kaerushi.monetify.BuildConfig
+
 const val SHARED_PREFS_NAME = "xposed_shared_prefs"
-const val PACKAGE_NAME = "com.kaerushi.monetify"
+const val PACKAGE_NAME = BuildConfig.APPLICATION_ID
 const val INSTAGRAM_PACKAGE_NAME = "com.instagram.android"
 const val PINTEREST_PACKAGE_NAME = "com.pinterest"
 const val REDDIT_PACKAGE_NAME = "com.reddit.frontpage"
