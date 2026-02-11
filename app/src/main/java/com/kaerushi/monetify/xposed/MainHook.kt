fd6ea73b4a1cc823e9b6947451098c02f5e1b906
KaeruShi <farel.ardiansyah404@gmail.com>
2026-02-11 07:41:21 +0700

    Enable resources hook

--- app/src/main/java/com/kaerushi/monetify/xposed/MainHook.kt
+++ app/src/main/java/com/kaerushi/monetify/xposed/MainHook.kt
@@ -3,7 +3,6 @@ package com.kaerushi.monetify.xposed
 import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
 import com.highcapable.yukihookapi.hook.factory.encase
 import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
-import com.kaerushi.monetify.R
 import com.kaerushi.monetify.data.PINTEREST_PACKAGE_NAME
 import com.kaerushi.monetify.data.REDDIT_PACKAGE_NAME
 import com.kaerushi.monetify.data.SUBSTRATUM_LITE_PACKAGE_NAME
@@ -11,21 +10,11 @@ import com.kaerushi.monetify.xposed.hooks.PinterestHooks
 import com.kaerushi.monetify.xposed.hooks.RedditHooks
 import com.kaerushi.monetify.xposed.hooks.SubstratumLiteHooks
 
-@InjectYukiHookWithXposed
-class MainHook : IYukiHookXposedInit {
+@InjectYukiHookWithXposed(isUsingResourcesHook = true)
+object MainHook : IYukiHookXposedInit {
     override fun onHook() = encase {
         loadApp(name = PINTEREST_PACKAGE_NAME, PinterestHooks)
         loadApp(name = REDDIT_PACKAGE_NAME, RedditHooks)
-        loadApp(name = SUBSTRATUM_LITE_PACKAGE_NAME) {
-            resources().hook {
-                injectResource {
-                    conditions {
-                        name = "no_theme_title"
-                        string()
-                    }
-                    replaceTo("kontol")
-                }
-            }
-        }
+        loadApp(name = SUBSTRATUM_LITE_PACKAGE_NAME, SubstratumLiteHooks)
     }
 }
