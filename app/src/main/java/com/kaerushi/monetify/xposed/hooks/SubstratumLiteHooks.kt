b728b53bde90c2444931eb0c7b61f74009f50610
KaeruShi <farel.ardiansyah404@gmail.com>
2026-02-17 19:37:28 +0700

    Refactor SubstratumLite color injection

--- app/src/main/java/com/kaerushi/monetify/xposed/hooks/SubstratumLiteHooks.kt
+++ app/src/main/java/com/kaerushi/monetify/xposed/hooks/SubstratumLiteHooks.kt
@@ -1,15 +1,9 @@
 package com.kaerushi.monetify.xposed.hooks
 
-import android.annotation.SuppressLint
 import android.app.Activity
-import android.content.Context
-import android.os.Bundle
-import android.view.View
-import com.highcapable.kavaref.KavaRef.Companion.resolve
 import com.highcapable.yukihookapi.hook.core.annotation.LegacyResourcesHook
 import com.kaerushi.monetify.R
 import com.kaerushi.monetify.data.SUBSTRATUM_LITE_PACKAGE_NAME
-import com.highcapable.yukihookapi.hook.log.YLog
 import com.kaerushi.monetify.xposed.utils.ColorUtils.colorPrimary
 import com.kaerushi.monetify.xposed.utils.ColorUtils.colorSurface
 import com.kaerushi.monetify.xposed.utils.ColorUtils.colorSurfaceContainer
@@ -19,7 +13,6 @@ import com.kaerushi.monetify.xposed.utils.PreferenceUtil.getAppMonetEnabled
 object SubstratumLiteHooks : BaseAppHook() {
     override val pkgName: String = SUBSTRATUM_LITE_PACKAGE_NAME
     override val duotoneDrawables: Map<String, Int> = mapOf(
-        "ic_appintro_arrow" to R.drawable.duotone_arrow_back,
         "ic_manager_info" to R.drawable.duotone_info,
         "nav_drawer_themes" to R.drawable.duoltone_starfilled,
         "nav_drawer_priorities" to R.drawable.duotone_pin,
@@ -31,10 +24,10 @@ object SubstratumLiteHooks : BaseAppHook() {
     }
 
     private fun applyMonetMainActivity(activity: Activity) {
-        injectColor("primary", "secondary") {
+        injectColor("primary", "primary_variant", "secondary", "secondary_variant") {
             colorPrimary(activity)
         }
-        injectColor("surface", "transparent", "background_material_light", "appintro_statusbar_color", "background_floating_material_light") {
+        injectColor("surface", "transparent", "background_material_light", "background_floating_material_light") {
             colorSurfaceContainer(activity)
         }
         injectColor("background") {
