bbc216e4ac96b5aa50f14327addeff421983e933
KaeruShi <farel.ardiansyah404@gmail.com>
2026-03-10 10:34:18 +0700

    Add support for disabling comment ads in Reddit AdsHook

--- app/src/main/java/com/kaerushi/monetify/xposed/hooks/reddit/AdsHook.kt
+++ app/src/main/java/com/kaerushi/monetify/xposed/hooks/reddit/AdsHook.kt
@@ -1,15 +1,27 @@
 package com.kaerushi.monetify.xposed.hooks.reddit
 
 import com.kaerushi.monetify.xposed.MainHook.bridge
+import org.luckypray.dexkit.query.enums.StringMatchType
 
 object AdsHook {
     fun RedditHooks.disableAds() {
+        val commentAds = bridge.findMethod {
+            matcher {
+                name("invokeSuspend")
+                declaredClass("LoadAdsCombinedCall", StringMatchType.Contains)
+            }
+        }
+        commentAds.single().getMethodInstance(appClassLoader!!).hook {
+            before {
+                result = args[0]
+            }
+        }
+
         val apsMethod = bridge.findClass {
             matcher {
                 usingStrings("AdPostSection(linkId=")
             }
         }.single().methods.single { it.isConstructor }
-
         val apsInit = apsMethod.getConstructorInstance(appClassLoader!!)
         val arg = apsInit.parameters.indexOfFirst { MutableList::class.java.isAssignableFrom(it.type) }
         apsInit.hook {
@@ -22,5 +34,6 @@ object AdsHook {
                     .set(sections, emptyArray<Any>())
             }
         }
+
     }
 }
